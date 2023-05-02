2f9db7368c830c6e1d6b325871ce9214d39c4126
SERVER\User1 <jeremeycalvin90@gmail.com>
2023-05-02 20:47:24 +0800

    first commit

---
+++ lib/infrastructure/models/request/search_product.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:riverpodtemp/infrastructure/services/local_storage.dart';
+
+class SearchProductModel {
+  final String text;
+  final int page;
+  SearchProductModel({
+    required this.text,
+    required this.page,
+  });
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map["search"] = text;
+    map["perPage"] = 10;
+    map["status"] = "published";
+    map["page"] = page;
+    map["lang"] = LocalStorage.instance.getLanguage()?.locale ?? "en";
+    return map;
+  }
+}
